04c8dd0d6854cdc4d4999899495cf34084df4b4f
Zachary Liu <liu.zachary@gmail.com>
2014-01-18 13:54:14 -0500

    Fixed missing date display when same as previous post.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <h2 class="fancytitle black">News Archives</h2>
         <?php while (have_posts()): the_post(); ?>
             <h3><?php the_title(); ?></h3>
-            <p><font size="-5">Posted on <?php the_date('F j, Y'); ?></font></p>
+            <p><font size="-5">Posted on <?php the_time('F j, Y'); ?></font></p>
             <?php the_content(); ?>
             <br /><br />
         <?php endwhile; ?>
