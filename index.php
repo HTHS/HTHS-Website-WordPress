94bc1b444c4b3677a961ac269688179e46caf815
Zachary Liu <liu.zachary@gmail.com>
2014-01-18 13:53:40 -0500

    Use home.php template for all post listings.

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php
-get_header();
+<?php get_header(); ?>
 
-get_sidebar();
-get_footer();
+<div id="content_left">
+    <div id="news" class="fancybox">
+        <h2 class="fancytitle black">News Archives</h2>
+        <?php while (have_posts()): the_post(); ?>
+            <h3><?php the_title(); ?></h3>
+            <p><font size="-5">Posted on <?php the_date('F j, Y'); ?></font></p>
+            <?php the_content(); ?>
+            <br /><br />
+        <?php endwhile; ?>
+    </div>
+
+    <?php if($pageLinks != ''): ?>
+        <div class="fancybox">
+            <?=$pageLinks?>
+        </div>
+    <?php endif; ?>
+</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
