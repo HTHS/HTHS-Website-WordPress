e133be3fc63ffa6730b3c85d3061e4c6df2c3e6a
Zachary Liu <liu.zachary@gmail.com>
2014-01-26 11:12:50 -0500

    Default to no widgets in sidebar.

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,3 @@
 <div id="sidebar">
-    <?php if (dynamic_sidebar('main_sidebar')) : else :
-        // Default to displaying the calendar and weather widgets
-        global $hths_widget_wrapper_args;
-        the_widget('HTHS_Calendar_Widget', null, $hths_widget_wrapper_args);
-        the_widget('HTHS_Weather_Widget', null, $hths_widget_wrapper_args);
-    endif; ?>
+    <?php dynamic_sidebar('main_sidebar'); ?>
 </div>
